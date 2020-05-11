483a6aa393ec4a1deff39e87824f59c85af7bc56
yasht96 <yashsurendra.trivedi@sjsu.edu>
2020-05-11 02:04:00 -0700

    add query

--- backend-mongo/src/schema/schema.js
+++ backend-mongo/src/schema/schema.js
@@ -1,6 +1,7 @@
 const graphql = require('graphql')
 const Employer = require('../models/employer')
 const Student = require('../models/student')
+const Job = require('../models/job')
 
 const {
     GraphQLObjectType,
@@ -52,6 +53,34 @@ const EmployerType = new GraphQLObjectType({
     })
 });
 
+const StudentApplication = new GraphQLObjectType({
+    name: 'StudentApplication',
+    fields: () => ({
+        _id: { type: GraphQLID },
+        studentId: { type: GraphQLID },
+        name: { type: GraphQLString },
+        university: { type: GraphQLString },
+        major: { type: GraphQLString },
+        cgpa: { type: GraphQLString }
+    })
+})
+
+const JobType = new GraphQLObjectType({
+    name: 'Job', 
+    fields: () => ({
+        _id: { type: GraphQLID },
+        companyName: { type: GraphQLString },
+        title: { type: GraphQLString },
+        postingDate: { type: GraphQLString },
+        deadline: { type: GraphQLString },
+        location: { type: GraphQLString },
+        salary: { type: GraphQLString },
+        jobDescription: { type: GraphQLString },
+        category: { type: GraphQLString },
+        students: { type: new GraphQLList(StudentApplication) }
+    })
+})
+
 const ApplicationsType = new GraphQLObjectType({
     name: 'Applications',
     fields: () => ({
@@ -87,7 +116,7 @@ const StudentType = new GraphQLObjectType({
         applications: { type: new GraphQLList(ApplicationsType) }
     })
 }); 
-        
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     description: 'Root Query',
@@ -103,12 +132,92 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args) {
                 return Student.find({});
             }
+        },
+        jobs: {
+            type: new GraphQLList(JobType),
+            resolve(parent, args) {
+                return Job.find({});        
+            }
+        },
+        employer: {
+            type: EmployerType,
+            args: { id: {type: GraphQLID }},
+            resolve(parent, args) {
+                return Employer.findOne({_id: args.id})
+            }
+        },
+        student: {
+            type: StudentType, 
+            args: { id: {type: GraphQLID }},
+            resolve(parent, args) {
+                return Student.findOne({_id: args.id})
+            }
+        },
+        job: {
+            type: JobType, 
+            args: { id: {type: GraphQLID }},
+            resolve(parent, args) {
+                return Job.findOne({_id: args.id})
+            }
+        },
+        companyJobs: {
+            type: new GraphQLList(JobType), 
+            args: { name: { type: GraphQLString }},
+            resolve(parent, args) {
+                return Job.find({companyName: args.name})
+            }
         }
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addStudent: {
+            type: StudentType,
+            args: {
+                name: { type: GraphQLString },
+                email: { type: GraphQLInt },
+                password: { type: GraphQLID },
+
+            },
+            resolve(parent, args) {
+                let author = {
+                    name: args.name,
+                    age: args.age,
+                    id: args.id
+                };
+                authors.push(author)
+                console.log("Authors", authors);
+                return author;
+            }
+        },
+
+        addBook: {
+            type: BookType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+                authorId: { type: GraphQLID },
+            },
+            resolve(parent, args) {
+                let book = {
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId,
+                    id: books.length+1
+                }
+                books.push(book);
+                return book;
+            }
+        }
+
+    }
+});
+
 const schema = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
 
 module.exports = schema;
